61cb9078ef377517491c4e094e074a2aab3e0221
Nobodip Debnath <nobodipdebnath45@gmail.com>
2025-11-18 22:21:40 +0600

    add file

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import ArticleCard from './ArticleCard';
+import { BsThreeDots } from "react-icons/bs";
+import { IoIosArrowForward } from "react-icons/io";
+
 
 const Article = () => {
     const [data, setData] = useState([]);
@@ -9,10 +12,21 @@ const Article = () => {
         .then(result => setData(result));
     }, [])
     return (
-        <div className='flex flex-col gap-8'>
-            {
-                data.map((card, idx) => <ArticleCard key={idx} card={card} ></ArticleCard>)
-            }
+        <div>
+            <div className='flex flex-col gap-8 mb-20'>
+                {
+                    data.map((card, idx) => <ArticleCard key={idx} card={card} ></ArticleCard>)
+                }
+            </div>
+            <div className='flex gap-4 items-center justify-center'>
+                <button className=' font-semibold px-6 text-lg border-gray-400 cursor-pointer hover:bg-black hover:text-white duration-500 hover:border-black text-black p-4 border rounded-xl'>1</button>
+                <button className=' font-semibold px-6 text-lg border-gray-400 cursor-pointer hover:bg-black hover:text-white duration-500 hover:border-black text-black p-4 border rounded-xl'>2</button>
+                <button className=' font-semibold px-6 text-lg border-gray-400 cursor-pointer hover:bg-black hover:text-white duration-500 hover:border-black text-black p-4 border rounded-xl'>3</button>
+                <button className=' font-semibold px-6 text-lg border-gray-400 cursor-pointer hover:bg-black hover:text-white duration-500 hover:border-black text-black p-4 border rounded-xl'>4</button>
+                <button className=' font-semibold px-6 text-lg border-gray-400 cursor-pointer hover:bg-black hover:text-white duration-500 hover:border-black text-black p-4 border rounded-xl'>5</button>
+                <button className=' font-semibold px-5 text-lg border-gray-400 cursor-pointer hover:bg-black hover:text-white duration-500 hover:border-black text-black py-5 border rounded-xl'><BsThreeDots></BsThreeDots></button>
+                <button className=' font-semibold px-5 text-lg border-gray-400 cursor-pointer hover:bg-black hover:text-white duration-500 hover:border-black text-black py-5 border rounded-xl'><IoIosArrowForward></IoIosArrowForward></button>
+            </div>
         </div>
     );
 };
