f7f5f26c188ded2ef229a8c10c10af3c6a2000de
Nobodip Debnath <nobodipdebnath45@gmail.com>
2025-11-19 13:05:45 +0600

    add all file

---
+++ src/components/Directions/Directions.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Directions = () => {
+    return (
+        <div>
+            <h1 className='text-3xl font-semibold text-black'>Directions</h1>
+            <h2 className='text-2xl font-semibold text-black mt-8'>1. Lorem ipsum dolor sit amet </h2>
+            <p className='text-base text-black/60 mt-8'>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+            <img className='w-full my-12' src="https://i.ibb.co.com/Q3qJw862/Rectangle-23.png" alt="" />
+            <p className='text-base text-black/60 '>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+            <hr className='my-12 text-gray-400' />
+            <h2 className='text-2xl font-semibold text-black mt-8'>2. Lorem ipsum dolor sit amet </h2>
+            <p className='text-base text-black/60 mt-8'>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+            <hr  className='my-12 text-gray-400'/>
+            <h2 className='text-2xl font-semibold text-black mt-8'>3. Lorem ipsum dolor sit amet </h2>
+            <p className='text-base text-black/60 mt-8'>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+            <hr  className='my-12 text-gray-400'/>
+        </div>
+    );
+};
+
+export default Directions;
--- src/components/Ingredients/Ingredients.jsx
+++ src/components/Ingredients/Ingredients.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Tasty from '../Tasty/Tasty';
+import Directions from '../Directions/Directions';
 
 const Ingredients = () => {
     return (
         <div className='grid grid-cols-3 gap-10'>
             <div className='col-span-2'>
-
+                <Directions></Directions>
             </div>
             <div className='col-span-1'>
                 <Tasty></Tasty>
